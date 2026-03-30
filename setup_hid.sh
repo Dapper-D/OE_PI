c8fca02dc9f978e77233425ce2e98204e5a9be54
Dappy <51823660+Dapper-D@users.noreply.github.com>
2026-03-30 15:37:35 -0700

    Refine comments for USB Gadget Mode setup
    Updated comments to clarify the purpose of each section.

--- setup_hid.sh
+++ setup_hid.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-# Initialize USB Gadget Mode
+# Configures the Pi as a Logitech Keyboard/Mouse Combo
 modprobe libcomposite
 cd /sys/kernel/config/usb_gadget/
 mkdir -p ghosthid && cd ghosthid
 
-# Spoof as a Logitech Device
+# Logitech IDs
 echo 0x046d > idVendor  
 echo 0xc31c > idProduct 
 mkdir -p strings/0x409
 echo "Logitech" > strings/0x409/manufacturer
 echo "USB Keyboard/Mouse Combo" > strings/0x409/product
 
-# Define Keyboard
+# Keyboard Setup
 mkdir -p functions/hid.usb0
 echo 1 > functions/hid.usb0/protocol
 echo 8 > functions/hid.usb0/report_length
 echo -ne \\x05\\x01\\x09\\x06\\xa1\\x01\\x05\\x07\\x19\\xe0\\x29\\xe7\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x08\\x81\\x02\\x95\\x01\\x75\\x08\\x81\\x03\\x95\\x05\\x75\\x01\\x05\\x08\\x19\\x01\\x29\\x05\\x91\\x02\\x95\\x01\\x75\\x03\\x91\\x03\\x95\\x06\\x75\\x08\\x15\\x00\\x25\\x65\\x05\\x07\\x19\\x00\\x29\\x65\\x81\\x00\\xc0 > functions/hid.usb0/report_desc
 
-# Define Mouse
+# Mouse Setup
 mkdir -p functions/hid.usb1
 echo 2 > functions/hid.usb1/protocol
 echo 4 > functions/hid.usb1/report_length
